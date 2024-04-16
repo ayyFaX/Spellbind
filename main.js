12531959818101f99807c650e392545e4e44ef6f
Adam <adam.m.roberts@sky.com>
2024-04-16 14:49:54 +0100

    Added guessing segment to gameplay and validation checks for guesses

--- main.js
+++ main.js
@@ -1,3 +1,4 @@
+//Function to move between pages
 function goToPage(pageUrl){
 	window.location.href = pageUrl;
 } 
@@ -22,9 +23,7 @@ startGame(){
 
 
 
-	document.getElementById('startBtn').style.display = 'none';
-	document.getElementById(
-
+	
 
 
 
@@ -52,6 +51,40 @@ updateGuessedLetters(){
 	document.getElementById('lettersGuessed').textContent = lettersGuessed.join(', ');
 }
 
+//Function to get guess from user and validate it
+guessLetter(){
+	//Defined as const as we do not want this change in its scope
+	const guess = document.getElementById('guessInput').value.toLowerCase();
+	//If validation fails return to exits function
+	if (!validateGuess(guess)) return;
+	
+	//If valid guess then enter into array
+	lettersGuessed.push(guess);
+	
+	let correctLetter = updateGuess(guess);
+	
+	//If letter not found in target word then handle then handle incorrect guess
+	if (!correctLetter) {
+				incorrectGuess();
+		}
+
+}
+
+//Function to validate user guesses
+validateGuess(guess){
+	//Check if guess is null,undefined or not an alphabetic letter regardless of case if so show error message
+	if (!guess || !guess.match(/[a-z]/i)) {
+        alert('Please enter a valid letter!');
+        return false;
+    }
+	//Check lettersGuessed array for previously guessed letters
+    if (lettersGuessed.includes(guess)) {
+        alert('You already guessed that letter!');
+        return false;
+    }
+    //If passes both validation then return true, showing validated guess
+	return true;
+}
 
 
 
@@ -67,3 +100,4 @@ updateGuessedLetters(){
 
 //Event listener to start game when start button clicked
 document.getElementById('startBtn').addEventListener('click', startGame);
+document.getElementById('guessBtn').addEventListener('click', guessLetter);
