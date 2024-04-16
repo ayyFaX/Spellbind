6e1b476efacce36375f361f0c8a6f0788cf1c714
Adam <adam.m.roberts@sky.com>
2024-04-16 11:15:09 +0100

    Added start button id and updated js code

--- main.js
+++ main.js
@@ -3,4 +3,30 @@ function goToPage(pageUrl){
 } 
 
 
+
+
+//One player Gameplay
+
+const words = ["tiger", "elephant", "lion", "gorilla", "sheep", "anteater", "giraffe", "seal", "bear", "panther"];
+
+
+startGame(){
+	targetWord=randomWord();
 	
+	
+
+
+
+
+
+
+
+
+
+}
+
+
+//Function to get random word from array
+randomWord(){
+	return words[Math.floor(Math.random() * words.length)];
+}
