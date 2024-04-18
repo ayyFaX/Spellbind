3884d46246dafbddb8ad483d37dcca09b4b1c94f
Adam <adam.m.roberts@sky.com>
2024-04-18 21:00:42 +0100

    Testing hangman image

--- main.js
+++ main.js
@@ -10,7 +10,7 @@ let targetWord;
 let currentGuess;
 let livesLeft;
 let handmanImageIndex;
-const hangmanPath = "https://github.com/ayyFaX/Spellbind/blob/b4793d439eb86fb9ae47266af9fd5236bc7397c8/";
+const hangmanPath = "hangman";
 let words;
 
 
@@ -98,7 +98,7 @@ function updateLivesLeft(){
 
 // Function to update hangman Image
 function updateHangmanImage(){
-	document.getElementById('hangmanImage').src = hangmanPath + "hangman" + hangmanImageIndex + ".png";
+	document.getElementById('hangmanImage').src = hangmanPath + hangmanImageIndex + ".png";
 }
 
 // Function to get guess from user and validate it
